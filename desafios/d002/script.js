9121995722269d30564aa61dffe7a9565925939b
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-15 17:11:32 -0300

    Create script.js
    função do desafio

---
+++ desafios/d002/script.js
@@ -0,0 +1,13 @@
+function startChallenge(){
+   var nome = prompt('Qual é o seu nome?');
+   var idade = parseInt(prompt(`Olá, ${nome}! Quantos anos você tem?`));
+
+   // * Implementei uma estrutura de repetição para caso a pessoa não digitar um numero
+   do{
+    alert('Por favor insira um numero');
+    idade = parseInt(prompt(`Olá, ${nome}! Quantos anos você tem?`));
+   }
+   while(isNaN(idade));
+
+    alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`);
+};
