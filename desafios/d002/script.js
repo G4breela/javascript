88cd1ebf1cee12ff26ad39f8778ac59fafe2b160
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-15 19:54:23 -0300

    Update script.js
    adicionei a função auxiliar, já que o do while sozinho está apresentando problema no navegador

--- desafios/d002/script.js
+++ desafios/d002/script.js
@@ -1,13 +1,24 @@
+function obterValor(mensagem){
+    // * variavel sem valor 
+    var valor;
+
+    // * Garante que o usuário insira o valor corretamente
+    do{
+        valor = Number(prompt(mensagem));
+
+        // ! se o valor for NaN, ou seja, não for um numero, exibe um alerta e pede novamente
+        if(isNaN(valor)){
+            alert('Por favor, digite um valor válido.');
+        }
+    }while (isNaN(valor));
+
+    // * retora o valor válido
+    return valor;
+}
+
 function startChallenge(){
    var nome = prompt('Qual é o seu nome?');
-   var idade = parseInt(prompt(`Olá, ${nome}! Quantos anos você tem?`));
-
-   // * Adicionei uma estrutura de repetição para caso a pessoa não digitar um numero
-   do{
-    alert('Por favor insira um numero');
-    idade = parseInt(prompt(`Olá, ${nome}! Quantos anos você tem?`));
-   }
-   while(isNaN(idade));
+   var idade = obterValor(`Olá, ${nome}! Quantos anos você tem?`);
 
     alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`);
 };
