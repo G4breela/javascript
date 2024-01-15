d62782dcb94ebc6a4de4a0311c0860b0fa4212c2
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-15 16:46:15 -0300

    Create script.js
    criando as funções

---
+++ desafios/d001/script.js
@@ -0,0 +1,11 @@
+function primeiroBotao(){
+    alert('Você clicou no 1° botão!')
+}
+
+function segundoBotao(){
+    alert('Você clicou no 2° botão!') 
+}
+
+function terceiroBotao(){
+    alert('Você clicou no 3° botão!') 
+}
