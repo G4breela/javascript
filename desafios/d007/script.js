de2a74007923893a0719e1d465a10c75ab48634c
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-28 18:24:40 -0300

    Update script.js

--- desafios/d007/script.js
+++ desafios/d007/script.js
@@ -1,15 +1,15 @@
 let cotDolar = parseFloat(prompt('Antes de qualquer coisa. Quanto está a cotação do dolar agora?'))
 let btn = document.querySelector("button");
 let resp1 = document.querySelector("h2");
-let resp2 = document.querySelector("p") 
+let resp2 = document.querySelector("p")
 
 
 
-btn.addEventListener('click', () =>{
+btn.addEventListener('click', () => {
     let valor = parseFloat(prompt("Quantos R$ você gostaria de converter?"));
 
     let convertido = valor / cotDolar;
 
-    resp1.innerText = `${valor.toLocaleString('pt-BR', {style: "currency", currency: 'BRL'})} corresponde a...`
-    resp2.innerText = `${convertido.toLocaleString('pt-BR', {style: "currency", currency: 'USD'})}`
+    resp1.innerText = `${valor.toLocaleString('pt-BR', { style: "currency", currency: 'BRL' })} corresponde a...`
+    resp2.innerText = `${convertido.toLocaleString('pt-BR', { style: "currency", currency: 'USD' })}`
 })
